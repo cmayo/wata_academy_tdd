b62f1bc67fa10c7b604c7cde89393244dfb09fdb
Carlos Mayo <c.mayo@wata.es>
2023-11-09 09:41:21 +0100

    controller test

---
+++ src/Application/UseCase/AddTask/AddTaskCommandHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\UseCase\AddTask;
+
+class AddTaskCommandHandler
+{
+    public function __invoke()
+    {
+        throw new \RuntimeException('Implement __invoke() method.');
+    }
+
+}
--- src/Infrastructure/Api/AddTaskController.php
+++ src/Infrastructure/Api/AddTaskController.php
@@ -2,11 +2,33 @@
 
 namespace App\Infrastructure\Api;
 
+use App\Application\UseCase\AddTask\AddTaskCommand;
+use App\Application\UseCase\AddTask\AddTaskCommandHandler;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class AddTaskController
 {
-    public function __invoke()
+    public function __construct(
+        protected AddTaskCommandHandler $addTaskCommandHandler
+    )
     {
-        throw new \RuntimeException('Implement __invoke() method.');
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $payload = $this->getPayload($request);
+        ($this->addTaskCommandHandler)(
+            new AddTaskCommand($payload['task'])
+        );
+
+        return (new JsonResponse())->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    protected function getPayload(Request $request): array
+    {
+        return json_decode($request->getContent(), true);
     }
 
 }
