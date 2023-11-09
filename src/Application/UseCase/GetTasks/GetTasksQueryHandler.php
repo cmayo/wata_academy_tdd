6ee3e2d7b836ddc474ee3497bf779aa539e5a206
Carlos Mayo <c.mayo@wata.es>
2023-11-09 10:42:40 +0100

    get tasks controller test

---
+++ src/Application/UseCase/GetTasks/GetTasksQueryHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\UseCase\GetTasks;
+
+class GetTasksQueryHandler
+{
+    public function __invoke()
+    {
+        throw new \RuntimeException('Implement __invoke() method.');
+    }
+
+}
