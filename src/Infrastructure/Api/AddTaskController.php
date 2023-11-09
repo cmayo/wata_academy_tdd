0e297ea16b69dc01af54474f9f3348aa2d5b8cf7
Carlos Mayo <c.mayo@wata.es>
2023-11-09 09:18:14 +0100

    acceptance test

---
+++ src/Infrastructure/Api/AddTaskController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Infrastructure\Api;
+
+class AddTaskController
+{
+    public function __invoke()
+    {
+        throw new \RuntimeException('Implement __invoke() method.');
+    }
+
+}
